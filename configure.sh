235c35e6bc519c5a96e4a78b2aa09b4b330c171a
KKD4yv1d <kkd4yv1d@gmail.com>
2022-08-05 01:24:01 -0300

    Fix workflow

--- configure.sh
+++ configure.sh
@@ -1,13 +1,13 @@
-sudo apt-get install -y libcurl4-openssl-dev
+apt-get install -y libcurl4-openssl-dev
 
 if [[ "$(command -v git)" == "" ]]; then
   echo "Package git not found! installing git..."
-  sudo apt install git -y > /dev/null
+  apt install git -y > /dev/null
 fi
 
 if [[ "$(command -v ar)" == "" ]]; then
   echo "Package binutils not found! installing binutils..."
-  sudo apt install binutils -y > /dev/null
+  apt install binutils -y > /dev/null
 fi
 
 DIR=$(pwd)
@@ -16,6 +16,6 @@ export PREFIX=/usr/local
 git clone https://github.com/Cogmasters/concord
 cd concord
 
-sudo make all install
+make all install
 
 cd $DIR
