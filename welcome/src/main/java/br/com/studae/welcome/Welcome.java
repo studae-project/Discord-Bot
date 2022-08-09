f056073825bcd3a6de231664d898a38a5c7c45a3
Victor Rodrigues <viictorawayz@gmail.com>
2022-08-09 17:08:02 -0300

    Change module package

--- welcome/src/main/java/br/com/studae/welcome/Welcome.java
+++ welcome/src/main/java/br/com/studae/welcome/Welcome.java
@@ -1,9 +1,8 @@
 package br.com.studae.welcome;
 
-import br.com.studae.commons.Module;
-import br.com.studae.commons.StudaeModule;
+import br.com.studae.commons.module.Module;
+import br.com.studae.commons.module.StudaeModule;
 import br.com.studae.welcome.listeners.UserJoinServerEvent;
-import net.dv8tion.jda.api.hooks.EventListener;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.List;
