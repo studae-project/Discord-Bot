4d652190b1a60d9eb066f9d847688f8311424a08
David Nery <davidnery@live.com>
2022-08-13 09:30:57 -0300

    Removidos módulos desnecessários

--- core/src/main/java/br/com/studae/discordbot/Main.java
+++ core/src/main/java/br/com/studae/discordbot/Main.java
@@ -1,43 +1,20 @@
 package br.com.studae.discordbot;
 
-import br.com.studae.commons.module.Module;
-import br.com.studae.commons.module.StudaeModule;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
-import org.reflections.Reflections;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
-
-    // do a smart cast and get all events registered!
-    public static List<Module> applicationModules = new Reflections("br.com.studae")
-            .getTypesAnnotatedWith(StudaeModule.class)
-            .stream().map(aClass -> {
-                try {
-                    return aClass.getDeclaredConstructors()[0].newInstance();
-                } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }).map(o -> (Module) o).collect(Collectors.toList());
 
     public static void main(String[] args) throws Exception {
 
         String token = System.getenv("TOKEN");
-
-        for (Module applicationModule : applicationModules) {
-            System.out.println(applicationModule);
-        }
 
         System.out.println("Iniciando bot discord...");
         long start = System.currentTimeMillis();
@@ -55,7 +32,7 @@ public class Main {
 
                     TextChannel channel = readyEvent.getJDA().getGuilds().get(0).getTextChannelsByName("geral", true).get(0);
                     channel.sendMessage("Olá pessoal da Studae, sou eu, o Studae Bot!").queue();
-                }).addEventListeners((Object[]) applicationModules.stream().flatMap(module -> module.getModuleEvents().stream()).toArray(ListenerAdapter[]::new));
+                });
 
         new Bot(builder.build());
     }
